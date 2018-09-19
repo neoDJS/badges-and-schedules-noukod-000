e6e712750dcaa7366e388dbb88b2efc31ba6d55a
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-19 15:44:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,16 @@ def batch_badge_creator(names_array)
   names_array.each { |e| badge_array << badge_maker(e) }
   badge_array
 end
+
+
+def assign_rooms(names_array)
+  assigned_room_array = []
+  names_array.each_with_index { |e, i| assigned_room_array << "Hello, #{e}! You'll be assigned to room #{i+1}!" }
+  assigned_room_array
+end
+
+def printer
+  names_array = []
+  puts batch_badge_creator(names_array)
+  puts assign_rooms(names_array)
+end
