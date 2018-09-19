55d9e7fefb61d778952a077a084eed491a7a8782
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-19 15:49:23 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+  badge = "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_array)
+  badge_array = []
+  names_array.each { |e| badge_array << badge_maker(e) }
+  badge_array
+end
+
+
+def assign_rooms(names_array)
+  assigned_room_array = []
+  names_array.each_with_index { |e, i| assigned_room_array << "Hello, #{e}! You'll be assigned to room #{i+1}!" }
+  assigned_room_array
+end
+
+def printer(names_array)
+  batch_badge_creator(names_array).each do |e| puts e end
+  assign_rooms(names_array).each do |e| puts e end
+end
